457c2703325fd3a28d7dd1b56e7190e3c85b959d
Duyzhii <duyzhii@gmail.com>
2024-11-20 12:21:29 +0700

    feat[home-page]: Add theme colors, footer, and change file extension to .hbs

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
 app.set("views", path.join("views"));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
