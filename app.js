deed6e6fbac2a6b5590e2727bf07eb6d8bf16a48
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-11-21 10:06:44 +0700

    chore: add fe hot reload, changed handlebar extension

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { engine } from "express-handlebars";
 
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -12,11 +15,28 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", path.join("views"));
+console.log(process.env.NODE_ENV);
 
-app.use(express.static("public"));
+// By default hot reload for views isn't supported, so using this instead
+if (process.env.NODE_ENV === "dev") {
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.watch(path.join(__dirname, "views"));
+
+    app.use(connectLivereload());
+
+    // Timeout to prevent premature reloads
+    liveReloadServer.server.once("connection", () => {
+        setTimeout(() => {
+            liveReloadServer.refresh("/");
+        }, 100);
+    });
+}
+
+app.engine("hbs", engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("home");
