0219b9b505e39bf43acb92e251323b3a063e04ae
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-12-01 18:21:54 +0700

    refactor: standardize import statement styles and remove unnecessary semicolons in route files

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,35 +1,32 @@
 // routes/user.routes.js
 
-import express from "express";
-import userController from "../controllers/user.controller.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
-import viewRenderer from "../utils/viewRenderer.js";
+import express from 'express'
+import userController from '../controllers/user.controller.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
+import viewRenderer from '../utils/viewRenderer.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.use(authMiddleware());
+router.use(authMiddleware())
 
 // @route   GET /api/v1/user/profile
 // @desc    Get user profile
-router.get(
-  "/profile",
-  viewRenderer("user/profile", userController.getUserProfile)
-);
+router.get('/profile', viewRenderer('user/profile', userController.getUserProfile))
 
 // @route   PUT /api/v1/user/profile
 // @desc    Update user profile
-router.put("/profile", userController.updateUserProfile);
+router.put('/profile', userController.updateUserProfile)
 
 // @route   GET /api/v1/user/change-password
 // @desc    Get change password view
-router.get("/change-password", viewRenderer("user/change-password"));
+router.get('/change-password', viewRenderer('user/change-password'))
 
 // @route   PUT /api/v1/user/change-password
 // @desc    Change user password
-router.put("/change-password", userController.changePassword);
+router.put('/change-password', userController.changePassword)
 
 // @route   DELETE /api/v1/user/delete
 // @desc    Delete user account
-router.delete("/delete", userController.deleteUser);
+router.delete('/delete', userController.deleteUser)
 
-export default router;
+export default router
--- routes/writer.routes.js
+++ routes/writer.routes.js
@@ -1,53 +1,44 @@
 // routes/writerRoutes.js
 
-import express from "express";
-import writerController from "../controllers/writer.controller.js";
-import articleController from "../controllers/article.controller.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
-import viewRenderer from "../utils/viewRenderer.js";
+import express from 'express'
+import writerController from '../controllers/writer.controller.js'
+import articleController from '../controllers/article.controller.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
+import viewRenderer from '../utils/viewRenderer.js'
 
-const router = express.Router();
+const router = express.Router()
 
 // Protected Routes for Writers
-router.use(authMiddleware(["writer"]));
+router.use(authMiddleware(['writer']))
 
 // @route   GET /api/v1/writer/
 // @desc    Get writer's profile
 
-router.get(
-  "/",
-  viewRenderer("writer/dashboard", writerController.getMyArticles)
-);
+router.get('/', viewRenderer('writer/dashboard', writerController.getMyArticles))
 
 // @route   GET /api/v1/writer/articles?status=(draft|pending|approved|rejected|published)
 // @desc    Get all articles by writer
-router.get("/articles", viewRenderer("writer/writer-articles"));
+router.get('/articles', viewRenderer('writer/writer-articles'))
 
 // @route   POST /api/v1/writer/articles
 // @desc    Create a new article
-router.post("/articles", writerController.createArticle);
+router.post('/articles', writerController.createArticle)
 
 // @route   GET /api/v1/writer/articles/:articleId
 // @desc    Get an article by ID
 
-router.get(
-  "/articles/:articleId",
-  viewRenderer("writer/article", articleController.getArticleById)
-);
+router.get('/articles/:articleId', viewRenderer('writer/article', articleController.getArticleById))
 
 // @route   PUT /api/v1/writer/articles/:articleId
 // @desc    Update an article
-router.put("/articles/:articleId", writerController.updateArticle);
+router.put('/articles/:articleId', writerController.updateArticle)
 
 // @route   DELETE /api/v1/writer/articles/:articleId
 // @desc    Delete an article
-router.delete("/articles/:articleId", writerController.deleteArticle);
+router.delete('/articles/:articleId', writerController.deleteArticle)
 
 // @route   PUT /api/v1/writer/articles/:articleId/submit
 // @desc    Submit article for approval
-router.put(
-  "/articles/:articleId/submit",
-  writerController.submitArticleForApproval
-);
+router.put('/articles/:articleId/submit', writerController.submitArticleForApproval)
 
-export default router;
+export default router
