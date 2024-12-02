c24e08951e39550f6920f18140bb37954e5fd0f1
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-12-02 20:52:42 +0700

    feat(app): integrate cookie-parser for enhanced session management; update cookie settings for security and add maxAge configuration

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import livereload from 'livereload'
 import connectLivereload from 'connect-livereload'
 import { engine } from 'express-handlebars'
 import { RedisStore } from 'connect-redis'
+import cookieParser from 'cookie-parser'
 
 // Custom modules
 import routes from './routes/index.routes.js'
@@ -21,7 +22,6 @@ import errorHandler from './middlewares/errorHandler.js'
 import passport from './config/passport.js'
 import redisClient from './utils/redisClient.js'
 import * as helpers from './helpers/handlebars.js'
-
 
 // Load environment variables
 dotenv.config()
@@ -31,6 +31,7 @@ const PORT = process.env.PORT || 3000
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const isDevelopment = process.env.NODE_ENV === 'dev'
+const maxAge = parseInt(process.env.JWT_EXPIRY, 10) || 1000 * 60 * 60 * 24 * 7 // Default to 7 days
 
 // Initialize Express app
 const app = express()
@@ -56,9 +57,16 @@ app.use(
     secret: process.env.SESSION_SECRET || 'your_session_secret',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: !isDevelopment },
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax', // Allow in dev
+      maxAge: 1000 * 60 * 30, // 30 minutes,
+    },
   }),
 )
+
+app.use(cookieParser())
 
 // Logging Middleware
 app.use(morgan(isDevelopment ? 'dev' : 'combined'))
@@ -67,7 +75,6 @@ app.use(morgan(isDevelopment ? 'dev' : 'combined'))
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(express.static(path.join(__dirname, 'public')))
 app.use('/uploads', express.static('uploads'))
 
 /* ---------- Handlebars Setup ---------- */
@@ -97,13 +104,22 @@ function setupLiveReload(app) {
 
 if (isDevelopment) setupLiveReload(app)
 
+// For debugging
+// app.use((req, res, next) => {
+//   console.log('Session ID:', req.sessionID)
+//   console.log('Session Data:', req.session)
+//   console.log('Authenticated:', req.isAuthenticated())
+//   console.log('User:', req.user)
+//   next()
+// })
+
 // Routes
 app.use('/', routes)
 
 // Error Handling
-app.use((req, res) => {
-  res.status(404).render('errors/404', { title: 'Page Not Found' })
-})
+// app.use((req, res) => {
+//   res.status(404).render('errors/404', { title: 'Page Not Found' })
+// })
 app.use(errorHandler)
 
 /* ---------- Start Server ---------- */
