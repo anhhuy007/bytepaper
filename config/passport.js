ecd3902468a8aa95b7a53166ca0e38b8f56c5829
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-12-06 15:24:03 +0700

    feat(auth): update Google authentication callback URL and enhance UI for password reset and signup forms

--- config/passport.js
+++ config/passport.js
@@ -58,7 +58,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/v1/auth/google/callback',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
